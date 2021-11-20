f90d8ef9f327220f9f54208a0256a59958f3f4b1
jgalin <jgalin@ttu.ee>
2021-11-20 14:40:03 +0200

    Add more iterations

--- models.py
+++ models.py
@@ -1032,8 +1032,8 @@ def train(
 
     save_epoch = epoch // 20 if epoch > 20 else 1
 
-    losses = np.zeros(1000000)
-    mean_losses = np.zeros(100000000)
+    losses = np.zeros(100000000)
+    mean_losses = np.zeros(1000000000)
     iter_ = 1
     loss_win, val_win = None, None
     val_accuracies = []
