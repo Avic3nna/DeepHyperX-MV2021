6cfb83a062415718bd6b5f72c844eed421ec7554
jgalin <jgalin@ttu.ee>
2021-11-20 17:07:05 +0200

    Changes in models.py and main.py for class balancing

--- main.py
+++ main.py
@@ -371,6 +371,7 @@ for run in range(N_RUNS):
         if CLASS_BALANCING:
             weights = compute_imf_weights(train_gt, N_CLASSES, IGNORED_LABELS)
             hyperparams["weights"] = torch.from_numpy(weights) # Add.cuda()??
+            hyperparams["weights"] = hyperparams["weights"].to(hyperparams["device"])
             weights = torch.from_numpy(weights)
             weights = weights.to(hyperparams["device"])
         # Neural network
