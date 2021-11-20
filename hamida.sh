0a3cbac457e1ae787f4358810b2ff53ee9fbbd73
jgalin <jgalin@ttu.ee>
2021-11-20 16:04:07 +0200

    Change hamida snow

--- hamida.sh
+++ hamida.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 main.py --model hamida --dataset WATER_HSI --training_sample 0.85 --epoch 50 --cuda 0  --class_balancing
+python3 main.py --model hamida --dataset SNOW_HSI --training_sample 0.85 --epoch 50 --cuda 0  --class_balancing
